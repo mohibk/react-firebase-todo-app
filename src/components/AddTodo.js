b63b2540468f6c302ced01c8f1547f6a8ea8a595
Mohib Khan <mohibk.in@gmail.com>
2021-06-21 17:08:25 +0530

    configure firebase

---
+++ src/components/AddTodo.js
@@ -0,0 +1,10 @@
+export default function AddTodo() {
+  return (
+    <div>
+      <form>
+        <input type="text" />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+}
